df50cd144bf5f0d72d3635632ddc4e78b020793d
DBDoco <dbdoco@gmail.com>
2024-01-25 20:10:59 +0100

    Add chat component.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from "../../components/sidebar/sidebar.component";
+import { ChatComponent } from '../../components/chat/chat.component';
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [SidebarComponent]
+    imports: [SidebarComponent, ChatComponent]
 })
 export class HomeComponent {
 
