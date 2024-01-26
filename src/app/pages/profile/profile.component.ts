7136b11a57cd18850f575efd20108e62278287c4
marinactonci <toncimarinac@gmail.com>
2024-01-26 02:59:06 +0100

    Created profile page component

---
+++ src/app/pages/profile/profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile.component.html'
+})
+export class ProfileComponent {
+
+}
