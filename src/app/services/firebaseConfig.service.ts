455094b808acea8274bfe2ff7679e1163fa2dc8a
marinactonci <toncimarinac@gmail.com>
2024-01-28 19:18:16 +0100

    Setting up prod environment variables.

--- src/app/services/firebaseConfig.service.ts
+++ src/app/services/firebaseConfig.service.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
