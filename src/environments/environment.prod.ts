aa822994ec56f612ad731832905434d8bd134136
marinactonci <toncimarinac@gmail.com>
2024-01-28 20:14:36 +0100

    Editing env variables.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'APIKEY',
-    authDomain: 'AUTHDOMAIN',
-    projectId: 'PROJECTID',
-    storageBucket: 'STORAGEBUCKET',
-    messagingSenderId: 'MESSAGINGSENDERID',
-    appId: 'APPID',
+    apiKey: 'AIzaSyAZ-oRKX-iT4DAiKr6SLtiBEIuHmi_aWag',
+    authDomain: 'chatapp-362ff.firebaseapp.com',
+    projectId: 'chatapp-362ff',
+    storageBucket: 'chatapp-362ff.appspot.com',
+    messagingSenderId: '717885049810',
+    appId: '1:717885049810:web:d688104aad58dd9ff8160b',
   },
 };
