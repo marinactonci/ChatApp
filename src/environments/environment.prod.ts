70fd0d8a8e84260fa03e8bbb20c2f023ada678ef
marinactonci <toncimarinac@gmail.com>
2024-01-28 20:02:39 +0100

    Editing env variables.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'FIREBASECONFIG_APIKEY',
-    authDomain: 'FIREBASECONFIG_AUTHDOMAIN',
-    projectId: 'FIREBASECONFIG_PROJECTID',
-    storageBucket: 'FIREBASECONFIG_STORAGEBUCKET',
-    messagingSenderId: 'FIREBASECONFIG_MESSAGINGSENDERID',
-    appId: 'FIREBASECONFIG_APPID',
+    apiKey: 'APIKEY',
+    authDomain: 'AUTHDOMAIN',
+    projectId: 'PROJECTID',
+    storageBucket: 'STORAGEBUCKET',
+    messagingSenderId: 'MESSAGINGSENDERID',
+    appId: 'APPID',
   },
 };
